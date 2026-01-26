49bfded6f7acc2005602b181bda22298ce4d7c40
Frlawer <frlawer@gmail.com>
2026-01-26 11:17:39 -0300

    Revert "Eliminar registros de Logger en funciones de obtención y eliminación de solicitudes"
    This reverts commit 7252c811deb083e7b2a2c8ccf67d3ee1fe19b7c7.

--- funciones.js
+++ funciones.js
@@ -668,6 +668,10 @@ function obtenerTodasSolicitudes() {
       id: String(data[i][12] || '')
     });
   }
+  Logger.log('Solicitudes obtenidas: ' + solicitudes.length);
+  if (solicitudes.length) {
+    Logger.log('Primera solicitud normalizada: ' + JSON.stringify(solicitudes[0]));
+  }
   
   return solicitudes;
 }
@@ -789,6 +793,7 @@ function eliminarSolicitud(id) {
     for (let i = 1; i < data.length; i++) {
       if (data[i][12] === id) { // Índice 12 = columna M (ID)
         sheet.deleteRow(i + 1); // +1 porque deleteRow es 1-indexed
+        Logger.log('Solicitud eliminada: ' + id);
         return { success: true };
       }
     }
@@ -999,6 +1004,8 @@ function guardarDatosCSV(datos) {
     for (let i = 1; i <= numColumnas; i++) {
       sheet.autoResizeColumn(i);
     }
+
+    Logger.log('Datos CSV guardados en SISO: ' + (numFilas - 1) + ' registros');
 
     return {
       success: true,
